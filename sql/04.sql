c4a0eb41f5db4bae5546a932e467921e246bf564
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2024-03-07 22:25:57 -0800

    updated

--- sql/04.sql
+++ sql/04.sql
@@ -3,3 +3,24 @@
  * 1. have appeared in at least one movie in the "Children" category,
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
+
+SELECT DISTINCT first_name, last_name FROM actor
+JOIN film_actor USING (actor_id)
+JOIN film ON film_actor.film_id = film.film_id
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category USING (category_id)
+WHERE actor.actor_id IN
+(SELECT actor.actor_id FROM actor
+JOIN film_actor USING (actor_id)
+JOIN film ON film_actor.film_id = film.film_id
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category USING (category_id)
+WHERE name = 'Children')
+AND actor.actor_id NOT IN
+(SELECT actor.actor_id FROM actor
+JOIN film_actor USING (actor_id)
+JOIN film ON film_actor.film_id = film.film_id                 
+JOIN film_category on film.film_id = film_category.film_id
+JOIN category USING (category_id)
+WHERE name = 'Horror')
+ORDER BY last_name;
--- sql/05.sql
+++ sql/05.sql
@@ -6,3 +6,11 @@
  * HINT:
  * This can be solved with a self join on the film_actor table.
  */
+
+SELECT f1.title FROM film f
+JOIN film_actor fa ON (f.film_id = fa.film_id)
+JOIN actor ON (fa.actor_id = actor.actor_id)
+JOIN film_actor fa1 ON (actor.actor_id = fa1.actor_id)
+JOIN film f1 ON (f1.film_id = fa1.film_id)
+WHERE f.title = 'AMERICAN CIRCUS'
+ORDER BY f1.title;
